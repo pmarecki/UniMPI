9c8ab92b16ff9bba213a41ba78a18e6fdada736b
P Marecki <pmarecki@gmail.com>
2013-04-12 16:39:01 +0200

    added simulated annealing

--- tsp.cc
+++ tsp.cc
@@ -9,19 +9,34 @@
 //yd = abs(y[i] - y[j]);
 //dij = nint(xd + yd);
 
+//Compute length of a tour given by `order[]` of nodes.
+float length(vector< vector<float> > &G, vi &order) {
+  int N = G.size();
+  float len = 0;
+  for(int t=1; t<N; ++t)
+    len += G[order[t-1]][order[t]];
+  return len;
+}
 
-//Shortest length and its node order.
-pair<double, vi> Optimal(vector<vector<double> > G) {
+//Take reference and swap nodes `i` and `j` in order.
+void swap_nodes(vi &order, int i, int j) {
+  int t = order[i];
+  order[i] = order[j];
+  order[j] = t;
+}
+
+
+
+//Returns shortest length and its node order.
+pair<float, vi> Optimal(vector<vector<float> > G) {
   int N = G.size();
   vi order(N);
   REP(i,N)
     order[i] = i;
   vi best;
-  double best_len = 1e10;   //infinity
+  float best_len = 1e10;   //infinity
   do {
-    double len = 0;
-    for(int t=1; t<N; ++t)
-      len += G[order[t-1]][order[t]];
+    float len = length(G,order);
     if (len < best_len) {
       best_len = len;
       vi tmp = order;
@@ -32,28 +47,26 @@ pair<double, vi> Optimal(vector<vector<double> > G) {
 }
 
 //declaration
-pair<double,vi> Refine(vector< vector<double> > G, vi order);
+pair<float,vi> Refine(vector< vector<float> > &G, vi order);
 
-pair<double, vi> Random1(vector<vector<double> > G, int count) {
+pair<float, vi> Random1(vector<vector<float> > G, int count) {
   srand(time(0));
   int N = G.size();
   vi order(N);
   REP(i,N)
     order[i] = i;
   vi best;
-  double best_len = 1e10;   //infinity
+  float best_len = 1e10;   //infinity
   REP(turn, count) {
     random_shuffle(order.begin() + 1, order.end());    //random order
-    double len = 0;
-    for(int t=1; t<N; ++t)
-      len += G[order[t-1]][order[t]];
+    float len = length(G, order);
     if (len < best_len) {
       best_len = len;
       vi tmp = order;
       best.swap(tmp);
 
     //------ Turn on / off local minimization by pair swapping.
-      pair<double,vi> res = Refine(G, best);
+      pair<float,vi> res = Refine(G, best);
       best_len = res.first;
       best.swap(res.second);
     ///-----
@@ -64,19 +77,17 @@ pair<double, vi> Random1(vector<vector<double> > G, int count) {
 
 // Attempt to refine solution by finding best order among orders
 // created from given "order" by swapping two sites.
-pair<double,vi> Refine(vector< vector<double> > G, vi order) {
+pair<float,vi> Refine(vector< vector<float> >& G, vi order) {
   int N = G.size();
   vi bestorder;
-  double best_len = 1e10;    //infinity
+  float best_len = 1e8;    //infinity
   for(int i=1; i<N; ++i)
     for(int j=1; j<N; ++j)
     {
       if (i==j) continue;
       vi ord = order;       //shuffled order
-      int t = ord[i];
-      ord[i] = ord[j];
-      ord[j] = t;
-      double len = 0;
+      swap_nodes(ord, i, j);
+      float len = 0;
       for(int t=1; t<N; ++t)
         len += G[ord[t-1]][ord[t]];
       if (len < best_len) {
@@ -90,36 +101,100 @@ pair<double,vi> Refine(vector< vector<double> > G, vi order) {
 
 
 
-int main() {
-  int N = 11;                                   //adjustable
-  int x[] = {0,1,2,3,4,4,4,4,4,4,4,4,4,4};      //triangle data //max N=14
-  int y[] = {0,0,0,0,0,1,2,3,4,5,6,7,8,9};
+////////////////////////////
+// Best algorithm.
+////////////////////////////
+//
+//  Uses only `length()` and `swap_nodes()` functions.
+//  Algorithm starts from random order, and attempts random swaps of nodes.
+//  After each swap: if solution is better than best it is accepted as best.
+//  If it is worse than best, than with some probability it is accepted.
+//  Probability drops exponentially with (length_current - lenght_best) / t,
+//  where "t" is the temperature.
+//
+//  Temperature is reduced by multiplying it with "factor" every "tmp_reduce"
+//  swaps of nodes.
+//
+pair<float, vi> Anneal(vector<vector<float> > &G, int count, int tmp_reduce,
+    float amplitude, float factor) {
+  srand(time(0));
+  int N = G.size();
+  //some initial order
+  vi order(N);
+  REP(i,N)
+    order[i] = i;
+  random_shuffle(order.begin() + 1, order.end());
+  vi best_order(order);
+  float best_len = length(G, order);
+  float t = 1.;
 
-  vector<double> empty(N);
-  vector<vector<double> > G(N,empty);       //table of distances
+  REP(turn, count) {
+    int i = rand() % N;
+    int j = rand() % N;
+    if (i==0 || j==0) continue;
+    vi order(best_order);
+    swap_nodes(order, i, j);      //make second swap if not accepted
+    float len = length(G, order);
+    if (len < best_len) {
+      best_len = len;
+      best_order.swap(order);
+    } else {
+      float p = (float)(rand() % 1000) / 1000;
+      if (exp((len - best_len) / t) < amplitude * p) {
+        best_len = len;
+        best_order.swap(order);
+      }
+    }
+    if (turn % tmp_reduce == 0) t *= factor;
+  }
+  return MP(best_len, best_order);
+}
+
+
+int main() {
+  int N = 19;                                   //adjustable
+  int x[] = {0,1,2,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};      //triangle data //max N=16
+  int y[] = {0,0,0,0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+  vector<float> empty(N);
+  vector<vector<float> > G(N,empty);       //table of distances
 
   REP(i,N)
     REP(j,N)
       G[i][j] = round(sqrt((x[i]-x[j])*(x[i]-x[j]) + (y[i]-y[j])*(y[i]-y[j])));
 
   printf("Distance table for N=%i nodes\n", N);
-  PrintMatrix(G,N,N);       //connections
+//  PrintMatrix(G,N,N);       //connections
   //complete search thru all points, "0" fixed?
 
   PosixStoper xx;
-  pair<double,vi> optimal = Optimal(G);
-  xx.Snap();
+  ////// Complete search
+//  pair<float,vi> optimal = Optimal(G);
+//  xx.Snap();
+//
+//  printf("Optimal tour (length=%3.2f):", optimal.first);
+//  PrintContainer(optimal.second);
+//  xx.PrintDt();
 
-  printf("Optimal tour (length=%3.2f):", optimal.first);
-  PrintContainer(optimal.second);
-  xx.PrintDt();
-
+  //////Fully random search
   xx.Snap();
   int count = 1e6;
-  pair<double,vi> rnd = Random1(G, count);
+  pair<float,vi> rnd = Random1(G, count);
   xx.Snap();
   printf("Best random (%i) tour (length=%3.2f):", count, rnd.first);
   PrintContainer(rnd.second);
+  xx.PrintDt();
+
+  ////// Simulated Annealing
+  xx.Snap();
+  int count_a = 1e5;
+  //synopsis: number of swaps (1e4), amplitude (=100),
+  //          temp. adjustment period (100), temp. multiplicator (0.99)
+  // every "adj.period" t*= multiplicator.
+  pair<float,vi> rnd_a = Anneal(G, count_a, 100, 100, 0.99);
+  xx.Snap();
+  printf("Best anneal (%i) tour (length=%3.2f):", count_a, rnd_a.first);
+  PrintContainer(rnd_a.second);
   xx.PrintDt();
 
 
